5f776658634b3b96d9b5d191889fe4b763f9a2ee
Hanamomon <145081281+Hanamomon@users.noreply.github.com>
2025-11-06 18:52:35 -0800

    feat: add new project dialog and logic

--- modules/dom/event.js
+++ modules/dom/event.js
@@ -6,6 +6,7 @@ export default class EventManager {
     }
 
     sidebarEvents() {
+        const addDialog = document.getElementById("project-add-dialog");
         this.sidebar.sidebarDiv.addEventListener("click", (e) => {
             if (e.target.tagName === "H2") {
                 this.mainContent.display("project-view");
@@ -15,7 +16,37 @@ export default class EventManager {
                 this.sidebar.display();
                 this.mainContent.display("project-todos");
             }
+            else if (e.target.id === "project-add-button")
+                addDialog.showModal();
         });
     }
 
+    addProjectModal() {
+        const addDialog = document.getElementById("project-add-dialog");
+        const projectName = addDialog.querySelector("input");
+        const confirmBtn = document.getElementById("confirm-btn");
+
+        confirmBtn.addEventListener("click", (e) => {
+            addDialog.close(projectName.value);
+        })
+
+        addDialog.addEventListener("close", () => {
+            if (addDialog.returnValue !== "default" && addDialog.returnValue !== "cancel") {
+                let projectDuplicate = false;
+                projectName.value = "";
+                this.projectHandler.projects.forEach((project) => {
+                    if (project.name === addDialog.returnValue) {
+                        alert("Project already exists!");
+                        projectDuplicate = true;
+                    }
+                });
+                if (!projectDuplicate && addDialog.returnValue.length > 2 && addDialog.returnValue.length < 20) {
+                    this.projectHandler.addProject(addDialog.returnValue);
+                    this.projectHandler.selectActiveProject(addDialog.returnValue);
+                    this.sidebar.display();
+                    this.mainContent.display("project-todos");
+                }
+            }
+        })
+    }
 }
